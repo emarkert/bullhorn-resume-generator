a1b46b6c9dc834fdf978fdbc190b609b7080e91c
sQuare <evanmarkert@gmail.com>
2020-06-14 20:32:26 -0400

    added variables + random generators to index.js

--- public/index.js
+++ public/index.js
@@ -1,2 +1,19 @@
-const randomNumber = Math.floor((Math.random * 20) + 1); 
+//get local JSON data for jobs
+let jobs = require('../data/jobs.json');
 
+//get and store length of jobs obj 
+let jobsNumber = Object.keys(jobs).length;
+
+// random number generator
+const randomNumber = Math.floor(Math.random() * 20) + 1;
+
+//random key get
+let randomKey = Object.keys(jobs)[Math.floor(Math.random()*Object.keys(jobs).length)]
+
+//random value get
+// console.log(jobs[Object.keys(jobs)[Math.floor(Math.random()*Object.keys(jobs).length)]]);
+
+//random job get
+let randomJob = jobs[Object.keys(jobs)[Math.floor(Math.random()*Object.keys(jobs).length)]]; 
+
+console.log(randomJob);
