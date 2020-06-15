18b2d109073bb598c94534c9896ead48b2ef1896
sQuare <evanmarkert@gmail.com>
2020-06-15 18:11:07 -0400

    commenting out helper code in index.js

--- public/index.js
+++ public/index.js
@@ -1,8 +1,8 @@
-//get local JSON data for jobs
+// get local JSON data for jobs
 let jobs = require('../data/jobs.json');
 
-//get and store length of jobs obj 
-let jobsNumber = Object.keys(jobs).length;
+// get and store length of jobs obj 
+// let jobsNumber = Object.keys(jobs).length;
 
 // random number generator
 const randomNumber = Math.floor(Math.random() * 20) + 1;
@@ -10,10 +10,10 @@ const randomNumber = Math.floor(Math.random() * 20) + 1;
 //random key get
 let randomKey = Object.keys(jobs)[Math.floor(Math.random()*Object.keys(jobs).length)]
 
-//random value get
+// random value get
 // console.log(jobs[Object.keys(jobs)[Math.floor(Math.random()*Object.keys(jobs).length)]]);
 
-//random job get
+//random job get based on jobs.json length
 let randomJob = jobs[Object.keys(jobs)[Math.floor(Math.random()*Object.keys(jobs).length)]]; 
 
 console.log(randomJob);
